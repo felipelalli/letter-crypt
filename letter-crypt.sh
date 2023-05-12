822fa409470e3892cd75a75925399419728d9354
Felipe Micaroni Lalli <micaroni@gmail.com>
2023-05-12 16:42:22 -0300

    First version.

---
+++ letter-crypt.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+version="1.0"
+
+usage(){
+    echo "Usage:"
+    echo ""
+    echo "$0 -e|--encrypt <password> [<msg>]   Encrypt a message."
+    echo "$0 -d|--decrypt <password> [<msg>]   Decrypt a message."
+    echo "$0 -h|--help                         Display this help message."
+    echo "$0 -v|--version                      Display the version number."
+    echo "Options:"
+    echo "-p|--paper-writable                    Format output for handwriting (default)."
+    echo "-m|--machine                           No formatting of the output."
+    echo "--                                     To separate the message in case it starts with '-'."
+    echo ""
+    echo "Examples:"
+    echo ""
+    echo "echo \"This is a message\" | $0 -e mypassword"
+    echo "echo \"53616c7465645f...\" | $0 -d mypassword"
+    echo "$0 -e mypassword -- -a"
+    echo "$0 --help"
+    echo "$0 --version"
+    exit 1
+}
+
+if [ $# -eq 0 ]; then
+    usage
+fi
+
+paper=true
+operation=""
+password=""
+message=""
+
+while (( "$#" )); do
+    case "$1" in
+        -e|--encrypt)
+            if [ -n "$operation" ]; then
+                usage
+            fi
+            operation="encrypt"
+            shift
+            password=$1
+            ;;
+        -d|--decrypt)
+            if [ -n "$operation" ]; then
+                usage
+            fi
+            operation="decrypt"
+            shift
+            password=$1
+            ;;
+        -h|--help)
+            usage
+            ;;
+        -v|--version)
+            echo $version
+            exit 0
+            ;;
+        -p|--paper-writable)
+            paper=true
+            ;;
+        -m|--machine)
+            paper=false
+            ;;
+        --)
+            shift
+            message=$1
+            ;;
+        -*|--*=)
+            echo "Error: Unsupported flag $1" >&2
+            usage
+            ;;
+        *)
+            if [ -n "$message" ]; then
+                usage
+            fi
+            message=$1
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$operation" ] || [ -z "$password" ]; then
+    usage
+fi
+
+if [ -z "$message" ]; then
+    read -d '' -r message
+fi
+
+if [ "$operation" = "encrypt" ]; then
+    encrypted=$(echo -n "$message" | openssl enc -aes-256-cbc -salt -iter 1000000 -pbkdf2 -pass pass:$password | xxd -p | tr -d '\n')
+    if $paper; then
+        printf "\nWrite down this to a paper:\n\nTo decrypt the message, please remove any numbers like 1), 2), 3), etc., at the start of each line, as well as any whitespaces and line breaks. Once you have cleaned the input, run the following command:\n\necho \"<hex>\" | xxd -r -p | openssl enc -aes-256-cbc -d -salt -iter 1000000 -pbkdf2 -pass pass:<password>\n\nReplacing <hex> with the encrypted message and <password> with the password. Or use the script \"$cmd\" with the option --decrypt.\n\n\n"
+        echo "$encrypted" | fold -w4 | paste - - -d ' ' | awk '{printf("%4d) %s\n", NR, $0)}'
+    else
+        echo "$encrypted"
+    fi
+else
+    clean_hex=$message
+    clean_hex=$(echo "$clean_hex" | grep -E '^[0-9a-fA-F )]*$')
+    clean_hex=$(echo "$clean_hex" | tr -d '[:blank:]')
+    clean_hex=$(echo "$clean_hex" | sed 's/^[0-9]*)//g')
+    clean_hex=$(echo "$clean_hex" | tr -d '\n')
+    if $paper; then
+    echo ""
+    fi
+    echo "$clean_hex" | xxd -r -p | openssl enc -aes-256-cbc -d -salt -iter 1000000 -pbkdf2 -pass pass:$password
+    if $paper; then
+    echo ""
+    fi
+fi
